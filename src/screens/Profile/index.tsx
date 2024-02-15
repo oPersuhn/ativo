46a82997facc1d95aa58ac461bf674edf8f6789b
oPersuhn <augusto_persuhn@estudante.sesisenai.org.br>
2024-02-15 16:18:02 -0300

    mostrar valores de forma organizada com porcentagens

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,36 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackNavigationProp } from '../../routes/types';
 import { AntDesign } from '@expo/vector-icons'; 
 import React, { useState, useEffect } from 'react';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { database } from '../../config/firebaseConfig';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default function Profile() {
   const navigation = useNavigation<RootStackNavigationProp>();
-  const [imgURL, setImageURL] = useState("")
-  const [progress, setProgess] = useState(0)
+  const [salario, setSalario] = useState('');
+  const [userData, setUserData] = useState<any | null>(null);
 
-  const handleUpload = (event: string) => {} 
+  useEffect(() => {
+    const auth = getAuth();
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          const userDoc = await getDoc(doc(database, 'users', user.uid));
+          const userData = userDoc.data();
+          setUserData(userData);
+        } catch (error) {
+          console.error('Erro ao obter dados do usuário:', error);
+        }
+      } else {
+        navigation.navigate("signin");
+      }
+    });
+
+    return () => {
+      unsubscribe(); // Limpar o listener quando o componente for desmontado
+    };
+  }, [navigation]);
+
+  const handleSaveSalario = async () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      // Caso o usuário não esteja autenticado, exiba uma mensagem de erro.
+      Alert.alert('Erro', 'Usuário não autenticado.');
+      return;
+    }
+
+    try {
+      // Atualizando o valor do salário no Firestore.
+      const userDocRef = doc(database, 'users', user.uid);
+      await setDoc(userDocRef, { salario: parseFloat(salario) }, { merge: true });
+      Alert.alert('Sucesso', 'Salário salvo com sucesso.');
+    } catch (error) {
+      console.error('Erro ao salvar o salário:', error);
+      Alert.alert('Erro', 'Ocorreu um erro ao salvar o salário.');
+    }
+  };
+
+  if (!userData) {
+    // Aguarde enquanto verifica o estado de autenticação e carrega os dados do usuário.
+    return null;
+  }
 
   return (
     <View style={styles.container}>
       <Text>Profile</Text>
       <StatusBar style="auto" />
         
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("new")}>
-              <AntDesign name="plus" size={24} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("report")}>
-              <Text>Report</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("config")}>
-              <Text>Configurações</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.profilePhotoContainer}>
-          
-          </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("new")}>
+          <AntDesign name="plus" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("report")}>
+          <Text>Report</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("config")}>
+          <Text>Configurações</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.profilePhotoContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Informe seu salário"
+          keyboardType="numeric"
+          value={salario}
+          onChangeText={text => setSalario(text)}
+        />
+        <TouchableOpacity style={styles.saveButton} onPress={handleSaveSalario}>
+          <Text style={styles.saveButtonText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -59,6 +118,25 @@ const styles = StyleSheet.create({
     shadowRadius: 2, // Raio da sombra
   },
   profilePhotoContainer : {
-
-  }
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    width: '80%',
+    borderRadius: 5,
+  },
+  saveButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+  },
+  saveButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
 });
--- src/screens/Report/index.tsx
+++ src/screens/Report/index.tsx
@@ -1,70 +1,127 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackNavigationProp } from '../../routes/types';
-import { collection, query, where, getDocs } from 'firebase/firestore';
-import { database } from '../../config/firebaseConfig';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, query, where, getDocs, getDoc, doc } from 'firebase/firestore';
+import { database } from '../../config/firebaseConfig'; // Importe a instância do Firestore
+import { getAuth } from 'firebase/auth';
 
 export default function Report() {
   const navigation = useNavigation<RootStackNavigationProp>();
   const [filteredGastos, setFilteredGastos] = useState<any[]>([]);
   const [userUid, setUserUid] = useState<string | null>(null);
-  const [selectedType, setSelectedType] = useState<string | null>(null); // Estado para armazenar o tipo de gasto selecionado
-
-  const handleFilterButtonClick = async () => {
-    try {
-      const auth = getAuth();
-      const user = auth.currentUser;
-      if (user) {
-        setUserUid(user.uid);
-        fetchFilteredGastos(user.uid); // Carrega os gastos filtrados para o usuário logado
-      } else {
-        navigation.navigate('signin'); // Redireciona para a tela de login se não houver usuário logado
-      }
-    } catch (error) {
-      console.error('Erro ao buscar gastos filtrados:', error);
-    }
-  };
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+  const [salario, setSalario] = useState<number>(0);
+  const [totalGastos, setTotalGastos] = useState<number>(0);
+  const [percentGastosFixos, setPercentGastosFixos] = useState<number>(0);
+  const [percentLazer, setPercentLazer] = useState<number>(0);
+  const [percentInesperado, setPercentInesperado] = useState<number>(0);
 
   const fetchFilteredGastos = async (uid: string) => {
     try {
-      const gastosCollection = collection(database, 'gastos');
+      const db = database; // Obtenha a instância do Firestore
+      const gastosCollection = collection(db, 'gastos'); // Obtenha a coleção 'gastos'
       let q = query(
         gastosCollection,
-        where('uid', '==', uid) // Filtra por UID do usuário logado
+        where('uid', '==', uid)
       );
 
-      // Se um tipo de gasto foi selecionado, adiciona uma cláusula de filtragem para o tipo de gasto
       if (selectedType) {
         q = query(q, where('tipoDeGasto', '==', selectedType));
       }
 
       const querySnapshot = await getDocs(q);
-      const filteredGastos = querySnapshot.docs.map((doc) => doc.data());
+      const filteredGastos = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        // Formatando o timestamp para 'dd/mm/aaaa'
+        data.timestamp = new Date(data.timestamp.seconds * 1000).toLocaleDateString('pt-BR');
+        return data;
+      });
       setFilteredGastos(filteredGastos);
+
+      // Calcular o total de gastos
+      const total = filteredGastos.reduce((acc, gasto) => acc + gasto.valor, 0);
+      setTotalGastos(total);
+
+      // Calcular as porcentagens de gastos em relação ao salário
+      const gastosFixos = filteredGastos.filter(gasto => gasto.tipoDeGasto === 'Fixo').reduce((acc, gasto) => acc + gasto.valor, 0);
+      const lazer = filteredGastos.filter(gasto => gasto.tipoDeGasto === 'Lazer').reduce((acc, gasto) => acc + gasto.valor, 0);
+      const inesperado = filteredGastos.filter(gasto => gasto.tipoDeGasto === 'Inesperado').reduce((acc, gasto) => acc + gasto.valor, 0);
+
+      setPercentGastosFixos((gastosFixos / salario) * 100);
+      setPercentLazer((lazer / salario) * 100);
+      setPercentInesperado((inesperado / salario) * 100);
     } catch (error) {
       console.error('Erro ao buscar gastos filtrados:', error);
     }
   };
 
+  useEffect(() => {
+    const fetchSalario = async () => {
+      try {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        if (user) {
+          // Obter o salário do usuário do Firestore
+          // Substitua 'users' e 'salaryField' pelo caminho correto no seu Firestore
+          const userDocRef = await getDoc(doc(database, 'users', user.uid));
+          const userData = userDocRef.data();
+          if (userData && userData.salario) {
+            setSalario(userData.salario);
+          }
+        }
+      } catch (error) {
+        console.error('Erro ao buscar salário do usuário:', error);
+      }
+    };
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user) {
+      setUserUid(user.uid);
+      fetchFilteredGastos(user.uid);
+      fetchSalario();
+    } else {
+      navigation.navigate('signin');
+    }
+  }, [selectedType]);
+
+  const handleFilterButtonClick = () => {
+    // Re-filtrar os gastos quando o botão de filtro for pressionado
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user) {
+      fetchFilteredGastos(user.uid);
+    }
+  };
+
+  const renderItem = ({ item }: { item: any }) => (
+    <View style={styles.item}>
+      <Text>Tipo de Gasto: {item.tipoDeGasto}</Text>
+      <Text>Data do Gasto: {item.timestamp}</Text>
+      <Text>Valor: {item.valor}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Report</Text>
-      {/* Botões para selecionar o tipo de gasto */}
+      <Text>Relatório de Gastos</Text>
       <Button title="Fixo" onPress={() => setSelectedType("Fixo")} />
       <Button title="Transporte" onPress={() => setSelectedType("Transporte")} />
       <Button title="Lazer" onPress={() => setSelectedType("Lazer")} />
       <Button title="Inesperado" onPress={() => setSelectedType("Inesperado")} />
-      {/* Botão para remover o filtro */}
       <Button title="Limpar Filtro" onPress={() => setSelectedType(null)} />
-      {/* Botão para aplicar o filtro */}
       <Button title="Filtrar" onPress={handleFilterButtonClick} />
-      {/* Exibir os gastos filtrados */}
-      {filteredGastos.map((gasto, index) => (
-        <Text key={index}>{JSON.stringify(gasto)}</Text>
-      ))}
+      <FlatList
+        data={filteredGastos}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <Text>Total Gasto: R$ {totalGastos.toFixed(2)}</Text>
+      <Text>Porcentagem de Gastos Fixos: {percentGastosFixos.toFixed(2)}%</Text>
+      <Text>Porcentagem de Gastos com Lazer: {percentLazer.toFixed(2)}%</Text>
+      <Text>Porcentagem de Gastos Inesperados: {percentInesperado.toFixed(2)}%</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -76,5 +133,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
